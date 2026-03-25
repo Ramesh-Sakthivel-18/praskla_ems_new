3852b49cf3139b8af9e15847b4b75d9a04348b24
Ramesh Sakthivel <rameshsakkthivell@gmail.com>
2026-03-25 18:11:59 +0530

    change in vite config ok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,17 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, '.'),
-        },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    server: {
-        port: 5173,
-    },
+  },
+  server: {
+    port: 5173,
+    allowedHosts: [
+      'ellis-bradytelic-factiously.ngrok-free.dev',
+      'ems.prasklatechnology.com'
+    ]
+  }
 })
